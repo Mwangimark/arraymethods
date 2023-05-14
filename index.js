007dc893e541918ae0baf3bceeb55da72c3a7520
Mwangimark <markcosmars2000@gmail.com>
2023-05-14 18:20:41 +0300

    for ech and push

---
+++ index.js
@@ -0,0 +1,26 @@
+ //array methods
+//2) push()
+let car = ["Benz","Wish","Fielder","Hilux","Corolla"]
+let bestcar = ["Mark X","Ford"]
+
+  car.push(...bestcar) //merge bestcararray to car array
+  console.log(car)
+  console.log(car.length)
+
+
+
+
+ //1) For each ()
+//  let n;
+//  let i;
+//  let array;
+
+//  car.forEach ((n,i,array) => console.log(n,i,array))
+ 
+   // n current element
+   // i index of the current element
+   // car array
+ 
+
+
+//  car.forEach(n=> console.log(n));
